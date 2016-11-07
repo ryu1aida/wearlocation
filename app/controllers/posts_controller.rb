7246745b12349bc9d26dc67e70788606b3bb25f0
Tomoya Yoshioka <tmy.ysok@gmail.com>
2016-11-07 16:52:35 +0900

    Fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,69 +1,51 @@
 class PostsController < ApplicationController
 
-def index
-  @posts = Post.includes(:user).page(params[:page]).per(5).order("created_at DESC")
-  @places = Place.all
-  @hash = Gmaps4rails.build_markers(@places) do |place, marker|
-    marker.lat place.latitude
-    marker.lng place.longitude
-    marker.infowindow place.name
+  def index
+    @posts = Post.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    @places = Place.all
+    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+      marker.lat place.latitude
+      marker.lng place.longitude
+      marker.infowindow place.name
+    end
   end
-end
 
-def new
- @post = Post.new
- @place = Place.new
-end
-
-
-def show
-  @post = Post.find(params[:id])
-
-end
-
-def create
-  current_user.posts.create(post_params)
-  # Post.create(
-  #   title: post_params[:title],
-  #   image: post_params[:image],
-  #   content: post_params[:content],
-  #   name: post_params[:name],
-  #   description: post_params[:description],
-  #   latitude: post_params[:latitude],
-  #   longitude: post_params[:longitude],
-  #   user_id: current_user.id
-  # )
-end
-# def create
-# Place.create(current_user.id )
-# end
-
-def destroy
-  post = Post.find(params[:id])
-  if post.user_id == current_user.id
-    post.destroy
+  def new
+   @post = Post.new
+   @place = Place.new
   end
-end
 
-def edit
-     @post = Post.find(params[:id])
-end
-
-def update
-  post = Post.find(params[:id])
-  if post.user_id == current_user.id
-    post.update(post_params)
+  def show
+    @post = Post.find(params[:id])
   end
-end
+
+  def create
+    current_user.posts.create(post_params)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.destroy
+    end
+  end
+
+  def edit
+       @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.update(post_params)
+    end
+  end
 
 
-private
-def post_params
-  params.require(:post)
-        .permit(:title, :image, :content, :name, :description, :latitude, :longitude)
-end
-  # def place_params
-  #   params.require(:place).permit()
-  # end
+  private
 
+    def post_params
+      params.require(:post)
+            .permit(:title, :image, :content)
+    end
 end
